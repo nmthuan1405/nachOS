67ac863391c729334d4d37d6dc4e7435094066cc
Trần Anh Huy <62178550+AnhHuy2001@users.noreply.github.com>
2021-10-09 22:33:13 +0700

    Add some system calls and programs
    2 system calls: PrintNum, PrintString
    2 programs: help.c, huytest.c

--- NachOS-4.0/code/userprog/syscall.h
+++ NachOS-4.0/code/userprog/syscall.h
@@ -37,6 +37,9 @@
 
 #define SC_Add		42
 
+#define SC_PrintNum 44
+#define SC_PrintString 49
+
 #ifndef IN_ASM
 
 /* The system call interface.  These are the operations the Nachos
@@ -58,6 +61,12 @@ void Halt();
  */ 
 
 int Add(int op1, int op2);
+
+// nhan vao mot so nguyen, in so nguyen do ra man hinh
+void PrintNum(int number);
+
+// nhan vao chuoi ky tu trong buffer, in chuoi do ra man hinh
+void PrintString(char buffer[]);
 
 /* Address space control operations: Exit, Exec, Execv, and Join */
 
@@ -105,8 +114,8 @@ typedef int OpenFileId;
  * the console device.
  */
 
-#define ConsoleInput	0  
-#define ConsoleOutput	1  
+#define ABCConsoleInput	0  
+#define ABCConsoleOutput	1  
  
 /* Create a Nachos file, with name "name" */
 /* Note: Create does not open the file.   */
