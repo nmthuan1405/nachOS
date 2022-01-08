cafd6b98277f504a4169b9f4936a00ecc959a99b
Nguyễn Minh Thuận <nmthuan.nmt@gmail.com>
2022-01-08 18:39:04 +0700

    change test

--- NachOS-4.0/code/test/pingpong.c
+++ NachOS-4.0/code/test/pingpong.c
@@ -4,12 +4,10 @@ int main()
 {
 	int pingPID, pongPID;
 
-	PrintString("Ping-Pong test starting ...\n");
+	// PrintString("Ping-Pong test starting ...\n");
 	
 	pingPID = Exec("./test/ping");
-	PrintNum(pingPID);
-	// pongPID = Exec("./test/pong");
+	pongPID = Exec("./test/pong");
 	Join(pingPID);
-	// Join(pongPID);
-	Exit(0);
+	Join(pongPID);
 }
