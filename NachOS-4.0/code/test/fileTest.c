94e3bb7f915f20d3e7a1fbd75af6c127af6134f9
nguyenminhuyen <nguyenminhuyen2001@gmail.com>
2022-01-09 18:03:18 +0700

    add test file

--- NachOS-4.0/code/test/fileTest.c
+++ NachOS-4.0/code/test/fileTest.c
@@ -2,16 +2,40 @@
 
 int main(){
     int result;
-    int fd;
+    int fd, type;
     char buffer[100];
     char* text = "Hello World!\n";
     
-    // result = CreateFile("test.txt");
+    // test syscall CreateFile
+    PrintString("Create testFile.txt\n");
+    result = CreateFile("./test/testFile.txt");
+    if (result == 0)
+        PrintString("Create file successfully.\n");
+    else
+        PrintString("Create file unsuccessfully.\n");
 
-    fd = Open("a.txt", 0);
-    Read(buffer, 100, fd);
-    Write(text, 5, fd);
-    PrintString(buffer);
+    // test syscall Open
+    PrintString("Open file...\nEnter type: ");
+    //type = ReadNum();
+    fd = Open("./test/testFile.txt", 0);
+
+    // write Hello World to testFile
+    Write(text, 20, fd);
+    //PrintString("Read from testFile: ");
+    Read(buffer, 20, fd);
+    PrintString(buffer); // print ko ra 
+
+    // test read write console
+    PrintString("\nEnter a text to write to opened file: ");
+    Read(buffer, 10, 0);
+    PrintString("The entered text is: ");
+    Write(buffer, 10, 1);
+
+    // write the entered text to file
+    Write(buffer, 10, fd);
+
+    // close file
+    Close(fd);
 
     Halt();
     return 0;
